11b7f2c4b78269d090219903930dbc59d16f7721
abdullahhas <abdullah312003@gmail.com>
2026-01-16 16:15:16 +0500

    understand MPPTmodel , one to many relation __str__ object methods ,  and make serializers

--- product_app/models.py
+++ product_app/models.py
@@ -1,14 +1,26 @@
 from django.db import models
-
+from mptt.models import MPTTModel , TreeForeignKey
 # Create your models here.
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
+    parent = TreeForeignKey('self' , on_delete=models.PROTECT , null=True , blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class MPPTMeta :
+        order_insertion_by = ['name']
 
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
 
 
 class Product(models.Model):
@@ -16,3 +28,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand , on_delete=models.CASCADE)
+    category = TreeForeignKey("Category" , on_delete=models.SET_NULL , null=True , blank=True)
+
+    def __str__(self):
+        return self.name
